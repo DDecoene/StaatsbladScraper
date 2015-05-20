d2fd70ab2047e03b8b7952cd26e1851d55cfcb3e
Dennis Decoene <dennis.decoene@gmail.com>
2015-05-20 20:58:02 +0200

    append to csv en geen titels in kolom

--- belg_scraper/pipelines.py
+++ belg_scraper/pipelines.py
@@ -13,37 +13,38 @@ from scrapy.contrib.exporter import CsvItemExporter
 import csv
 import re
 
+
 class BelgScraperPipeline(object):
     def process_item(self, item, spider):
-        #Cleaning of name'
+        # Cleaning of name'
         m = re.search('^(.+)\s(.+),.+\xa0\xa0.+$', item["name"])
         b = re.search('^(.+)\xa0\xa0(.+)$', item["name"])
 
         if m:
-            #print m.group(1)
+            # print m.group(1)
             item["name"] = m.group(1) + " " + m.group(2)
         elif b:
-            #print b.group(1)    
-            item["name"] = b.group(1) + " " + b.group(2) 
-        
-        #Cleaning of address, zip, city
-        m = re.search('(\d\d\d\d)\s(.+)\s$',item["city"])
-        item["city"]=m.group(2)
-        
-        m = re.search('(\d\d\d\d)',item["zipcode"])
-        item["zipcode"]=m.group(1)
+            # print b.group(1)
+            item["name"] = b.group(1) + " " + b.group(2)
 
-        m = re.search('^\n(.+)\s(\d\d\d\d)\s(.+)\s$',item["address"])
+            # Cleaning of address, zip, city
+        m = re.search('(\d\d\d\d)\s(.+)\s$', item["city"])
+        item["city"] = m.group(2)
+
+        m = re.search('(\d\d\d\d)', item["zipcode"])
+        item["zipcode"] = m.group(1)
+
+        m = re.search('^\n(.+)\s(\d\d\d\d)\s(.+)\s$', item["address"])
 
         if m:
-            item["address"]=m.group(1)
+            item["address"] = m.group(1)
         else:
-            item["address"]=""
+            item["address"] = ""
 
-        m = re.search('(\d\d\d\d-\d\d-\d\d)',item["date"])
+        m = re.search('(\d\d\d\d-\d\d-\d\d)', item["date"])
         item["date"] = m.group(1)
-        
-        m = re.search('^(\d\d\d\.\d\d\d\.\d\d\d).+$',item["number"])
+
+        m = re.search('^(\d\d\d\.\d\d\d\.\d\d\d).+$', item["number"])
         if m:
             item["number"] = m.group(1)
         else:
@@ -51,11 +52,11 @@ class BelgScraperPipeline(object):
 
         return item
 
-class CSVPipeline(object):
 
+class CSVPipeline(object):
     def __init__(self):
         self.files = {}
-        
+
     @classmethod
     def from_crawler(cls, crawler):
         pipeline = cls()
@@ -64,13 +65,14 @@ class CSVPipeline(object):
         return pipeline
 
     def spider_opened(self, spider):
-        file = open("{}_{}_{}__{}_{}_{}.csv".format(spider.pdda,spider.pddm,spider.pddj,spider.pdfa,spider.pdfm,spider.pdfj), 'a+b')
+        file = open("{}_{}_{}__{}_{}_{}.csv".format(spider.pdda, spider.pddm, spider.pddj, spider.pdfa, spider.pdfm,
+                                                    spider.pdfj), 'a+b')
         self.files[spider] = file
         kwargs = {}
         kwargs['delimiter'] = ';'
         kwargs['quoting'] = csv.QUOTE_ALL
-        self.exporter = CsvItemExporter(file,**kwargs)
-        self.exporter.fields_to_export = ["name","address","zipcode","city","number","date"]
+        self.exporter = CsvItemExporter(file, include_headers_line=False, **kwargs)
+        self.exporter.fields_to_export = ["name", "address", "zipcode", "city", "number", "date"]
         self.exporter.start_exporting()
 
     def spider_closed(self, spider):
